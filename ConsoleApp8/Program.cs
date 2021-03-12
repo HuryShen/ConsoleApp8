ee410e48f4b41ce7507c17cf4b9031659d0eb650
hury@hanxia.onmicrosoft.com <hury@hanxia.onmicrosoft.com>
2021-03-12 14:32:53 +0800

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+            .Create("clientId")
+            .WithTenantId("tenantId")
+            .WithClientSecret("clientSecret")
+            .Build();
+
+            ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var result = await graphClient.Users
+                    .Request()
+                    .Select(e => new
+                    {
+                        e.DisplayName,
+                        e.Id,
+                        e.Identities
+                    })
+                    .GetAsync();
+
+            foreach (var user in result.CurrentPage)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(user));
+            }
+            Console.ReadLine();  // add this line or use "Ctrl + F5" to run the code
+        }
+    }
+}
